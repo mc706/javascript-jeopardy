0e5b004582a07158541e078362cb062395624cb2
Ryan McDevitt <rmcdevitt@foxgrp.net>
2014-03-22 15:15:28 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+javascript jeopardy
+===================
+
+Javascript Jeopardy is a single page, self contained, jeopardy board rendered using Twitter Bootstrap and Jquery.
+
+The categories, questions, answers, and scoring all driven by the board.json file.
+
+builder.html is a page build in Twitter Bootstrap and Angular.JS for creating and editing the board.json file.
+
+Demo
+----
+[Demo] (http://mc706.github.io/javascript-jeopardy/index.html)
---
+++ js/scripts.js
@@ -0,0 +1,84 @@
+$(function(){
+    $.ajax({
+        'async': false,
+        'global': false,
+        type:'GET',
+        dataType:'json',
+        url:'board.json',
+        success:function(data){
+            map = data;
+            loadBoard();
+        }
+    });
+    $('.unanswered').click(function(){
+        var category = $(this).parent().data('category');
+        var question = $(this).data('question');
+        var value = map[category].questions[question].value;
+        var answers = $('#answers');
+        $('.modal-title').empty().text(map[category].name);
+        $('#question').empty().text(map[category].questions[question].question);
+        answers.empty();
+        $.each(map[category].questions[question].answers, function(i, answer){
+            answers.append(
+                '<button class="btn btn-danger answer" ' +
+                    'data-category="'+category+'"' +
+                    'data-question="'+question+'"' +
+                    'data-value="'+value+'"' +
+                    'data-correct="'+answer.correct+'"' +
+                    '>'+ answer.text+'</button><br><br>'
+            )
+        });
+        $('#question-modal').modal('show');
+        console.log(category, question);
+        console.log(map[category].questions[question]);
+        handleAnswer();
+    });
+
+});
+var score = 0;
+var map;
+function loadBoard(){
+    var board = $('#main-board');
+    var columns = map.length;
+    var column_width = parseInt(12/columns);
+    console.log(columns);
+    $.each(map, function(i,category){
+        //load category name
+        var header_class = 'text-center col-md-' + column_width;
+        if (i === 0 && columns % 2 != 0){
+            header_class += ' col-md-offset-1';
+        }
+        $('.panel-heading').append(
+            '<div class="'+header_class+'"><h4>'+category.name+'</h4></div>'
+        );
+        //add column
+        var div_class = 'category col-md-' + column_width;
+        if (i === 0 && columns % 2 != 0){
+            div_class += ' col-md-offset-1';
+        }
+        board.append('<div class="'+div_class+'" id="cat-'+i+'" data-category="'+i+'"></div>');
+        var column = $('#cat-'+i);
+        $.each(category.questions, function(n,question){
+            //add questions
+            column.append('<div class="well question unanswered" data-question="'+n+'">'+question.value+'</div>')
+        });
+    });
+    $('.panel-heading').append('<div class="clearfix"></div>')
+
+}
+
+function updateScore(){
+    $('#score').empty().text(score);
+}
+
+function handleAnswer(){
+    $('.answer').click(function(){
+        var tile= $('div[data-category="'+$(this).data('category')+'"]>[data-question="'+$(this).data('question')+'"]')[0];
+        $(tile).empty().removeClass('unanswered').unbind().css('cursor','not-allowed');
+        if ($(this).data('correct')){
+            score += parseInt($(this).data('value'));
+        }
+        $('#question-modal').modal('hide');
+        updateScore();
+    })
+}
